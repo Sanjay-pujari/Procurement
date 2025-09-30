6da0970642b2b0aea8b1965d12d933c7f508c6a3
sanjay-pujari <sanjay@scopicsolutions.com>
2025-09-30 17:44:49 +0530

    resolve HttpClientModule issue

--- procurepro-web/src/app/app.config.ts
+++ procurepro-web/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token.interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
